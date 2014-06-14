4cc441504f08b9acc2cb0e10d6609257c1335ce2
junsuwhy <junsuwhy@netivism.com.tw>
2014-06-15 00:28:32 +0800

    Add wiji Library

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 
 $.extend( $.fn ,{
+	wjin:function(anistyle,s){
+		s = isNaN(parseFloat( s ))? 0 : parseFloat( s );
+		// console.log(this);
+		var $obj = this;
+		$obj.each(function(){
+			var $this = $(this);
+			// console.log($this);
+			$this.css('opacity',0);
+			var trig = function(e) {
+				// console.log($(window).scrollTop());
+				if(($this.offset().top - $(window).scrollTop())<($(window).height()-$this.height())){
+					setTimeout(function(){
+						offscroll(trig);
+						$this.css('opacity','').addClass('animated').addClass(anistyle);
+						setTimeout(function(){
+							$this.removeClass(anistyle).removeClass('animated');
+						}, 1000);
+					}, s);
+				}
+			}
+			$(window).on("scroll",trig);
+
+			function offscroll(trig){
+				$(window).off("scroll",trig);
+			}
+		});
+	},
 	anifin:function(func){
 		this.each(function(){
 			this.addEventListener("animationend",func);
@@ -14,30 +41,19 @@ $.extend( $.fn ,{
 		y=isNaN(parseFloat(y))?0:parseFloat(y);
 		var x = this;
 		x.css({
-			"background-position-y": y
+			"background-position-y": y,
+			"background-position":"0 "+y+"px"
 		});
 		$(window).scroll(function(event) {
 			x.css({
 				"background-position-y": function(){
 					return $(window).scrollTop()-x.offset().top+y;
+				},
+				"background-position":function(){
+					var posy = $(window).scrollTop()-x.offset().top+y;
+					return "0 "+posy+"px";
 				}
 			});
-		});
-	},
-	/**
-	 * 製造滑上來時，翻滾出現的動作
-	 * @param  {Number} s 晚幾毫秒
-	 * @return {[type]}   [description]
-	 */
-	ff:function(s){
-		s=isNaN(parseFloat(s))?0:parseFloat(s);
-		var x = this;
-		$(window).scroll(function(event) {
-			if((x.offset().top - $(window).scrollTop())<($(window).height()-x.height())){
-				setTimeout(function(){
-					x.removeClass('hide').addClass('flipInX');
-				}, s);
-			}
 		});
 	},
 	/**
@@ -54,18 +70,9 @@ $.extend( $.fn ,{
 
 					$obj.addClass('animated').addClass(anistyle);
 					setTimeout(function(){
-						$obj.removeClass(anistyle);
+						$obj.removeClass(anistyle).removeClass(anistyle);
 					}, 1000);
 				},function(){}
-			)
-			$obj.click(
-				function(){
-
-					$obj.addClass('animated').addClass(anistyle);
-					setTimeout(function(){
-						$obj.removeClass(anistyle);
-					}, 1000);
-				}
 			)
 		});
 	},
@@ -78,7 +85,7 @@ $.extend( $.fn ,{
 
 					$obj.addClass('animated').addClass(anistyle);
 					setTimeout(function(){
-						$obj.removeClass(anistyle);
+						$obj.removeClass(anistyle).removeClass(anistyle);
 					}, 1000);
 				}
 			)
@@ -115,13 +122,19 @@ $('.circle').hover(function(){
 	}, 1000);
 });
 
-if($(window).width()>960)$('.header').xp();
+if($(window).width()>960){
+	$('.header').xp();
+}
+
 for (var i = 1; i < 4; i++) {
 	var j=($(window).width()<960)?0:i;
-	$('.step'+i).ff(j*300);
+	$('.step'+i).wjin('flipInX',j*300);
 };
+$('.position img').wjin('lightSpeedIn');
+
 
 $('.link>div').hoverAni('rubberBand');
+$('.link>div').clickAni('rubberBand');
 setInterval(function(){
 	$('.position img').addClass('animated').toggleClass('bounce');
 },2000);
